8b403eecaf09304a9346d156372015394ec72a62
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-01-26 14:22:52 +0100

    Correccion ejercicio 3_3. Length mal escrito

--- ejercicio3/ejercicio3_3.js
+++ ejercicio3/ejercicio3_3.js
@@ -2,18 +2,17 @@ const input = "string";
 
 const inverseCount = (string) => {
     const strList = [...string].reverse();
-    const listLength = strList.length;
     strInverse = strList.join("");
-    return(`${listLength} ${strInverse}`);
+    return(`${strList.length} ${strInverse}`);
 }
 
 inverseCount(input); // "6 gnirts"
 console.log(inverseCount(input));
 
-
 const input2 = "variable";
 inverseCount(input2); // "8 elbairav"
 console.log(inverseCount(input2));
+
 
 const input3 = "pointer";
 inverseCount(input3); // "7 retniop"
